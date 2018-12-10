1079009e11ce13bea9ea9239586ff3fc6cf823bb
Joseph Pena <josephpendev@gmail.com>
2018-12-09 18:30:35 -0800

    fk

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var app = express();
 var path = require("path");
 
 app.use("/dist", express.static(__dirname + "/dist"));
-
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
